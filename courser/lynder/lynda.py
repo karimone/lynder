852e2501979ec74c6788ba561fd3097e429535d4
Karim N. Gorjux <karimone@gmail.com>
2020-09-28 08:03:13 +1000

    WIP

--- courser/lynder/lynda.py
+++ courser/lynder/lynda.py
@@ -103,6 +103,7 @@ class LyndaTutorialData:
             return func()
         raise NotImplemented(f'The {context_file} for {format} is not implemented')
 
+
 class LyndaTutorial:
 
     def __init__(self, page_data: str):
@@ -148,7 +149,69 @@ class LyndaTutorial:
         return tutorial
 
 
+
+class LyndaAuthorCourse:
+    def __init__(self, author: str, title: str, duration: str, release: str, url: str, description: str):
+        self.author = author
+        self.title = title
+        self.duration = duration
+        self.release = release
+        self.url = url
+        self.description = description
+
+
+class LyndaAuthorData:
+    def __init__(self, author: str, courses: List[LyndaAuthorCourse]):
+        self.author = author
+        self.courses = courses
+
+
+class LyndaAuthor:
+    def __init__(self, page_data: str):
+        self._page_data = page_data
+        self._soup = BeautifulSoup(self._page_data, "html.parser")
+        self._author = None
+
+    def _get_author(self) -> Optional[str]:
+        if self._author:
+            return self._author
+        breadcrumbs = self._soup.find('div', attrs={'class': 'breadcrumbs'})
+        if not breadcrumbs:
+            return None
+        all_author = breadcrumbs.findChild('a').text.strip()
+        if all_author.lower() != "All Authors".lower():
+            return None
+        author = breadcrumbs.findChild('span').text.strip()
+        self._author = author
+        return author
+
+    def _get_courses(self) -> List[LyndaAuthorCourse]:
+        author = self._get_author()
+        result = []
+        courses = self._soup.find_all("li", attrs={"data-result-type": "COURSE"})
+        for course in courses:
+            a = course.findChild("a")
+            url = a.attrs['href']
+            title = course.find("h3").text.strip()
+            duration = course.find("span", attrs={"class": "meta-duration"}).text.strip()
+            release = course.find("span", attrs={"class": "release-date"}).text.strip()
+            description = course.find("div", attrs={"class": "meta-description"}).text.strip()
+            result.append(LyndaAuthorCourse(author=author,
+                                            url=url,
+                                            title=title,
+                                            duration=duration,
+                                            release=release,
+                                            description=description))
+        return result
+
+    def get_author_data(self) -> LyndaAuthorData:
+        return LyndaAuthorData(author=self._get_author(), courses=self._get_courses())
+
+
 class Lynda:
+
+    AUTHOR_PAGE = 'author'
+    TUTORIAL_PAGE = 'tutorial'
 
     def __init__(self, url: str):
         self._url = url
@@ -163,37 +226,48 @@ class Lynda:
         self._page_data = response.text
         self._soup = BeautifulSoup(self._page_data, "html.parser")
 
-    def _analyse_for_author_page(self) -> Optional[str]:
+    def _determine_type_of_page(self) -> Optional[str]:
         """Returns the author name or nothing"""
         breadcrumbs = self._soup.find('div', attrs={'class': 'breadcrumbs'})
         if not breadcrumbs:
-            return None
+            return Lynda.TUTORIAL_PAGE
         all_author = breadcrumbs.findChild('a').text.strip()
         if all_author.lower() != "All Authors".lower():
             return None
         author = breadcrumbs.findChild('span').text.strip()
-        return author.title()
+        return Lynda.AUTHOR_PAGE
 
     def parse_url(self):
-        author = self._analyse_for_author_page()
-        print(f"Parsed url. Author: {author}")
-        if author:
-            self.run_author_download(author)
-        else:
+        type_of_page = self._determine_type_of_page()
+        if type_of_page == Lynda.AUTHOR_PAGE:
+            self.run_author_download()
+        elif type_of_page == Lynda.TUTORIAL_PAGE:
             self.run_tutorial_download(self._url)
 
-    def run_author_download(self, author: str):
-        # get every tutorial
-        #   run the download
+    def run_author_download(self):
 
-        courses = self._soup.find_all("li", attrs={"data-result-type": "COURSE"})
-        print(f">>> Download for {author}")
-        author_slugified = get_or_create_folder(path=os.getcwd(), folder_name=author)
+        lynda_author = LyndaAuthor(page_data=self._page_data)
+        lynda_author_data = lynda_author.get_author_data()
+
+        author_slugified = get_or_create_folder(path=os.getcwd(), folder_name=lynda_author_data.author)
         os.chdir(author_slugified)
-        for n, course in enumerate(courses, start=1):
-            a = course.findChild("a")
-            url = a.attrs['href']
-            self.run_tutorial_download(url)
+        total_courses = len(lynda_author_data.courses)
+
+        info_text = f"# Author {lynda_author_data.author}\n\n"
+        info_text += f"Full download of author courses\n"
+
+        for n, course in enumerate(lynda_author_data.courses, start=1):
+            print(f"[{n}/{total_courses}] > {course.title}")
+            self.run_tutorial_download(course.url)
+            info_text += f"\n## {course.title}\n\n"
+            info_text += f"\t - **Duration:** {course.duration}\n"
+            info_text += f"\t - **Release:** {course.release}\n"
+            info_text += f"\t - **Description:** {course.description}\n"
+            info_text += f"\t - **Url:** {course.url}\n\n"
+
+        info_file = open("INFO.md", "w")
+        info_file.write(info_text)
+        info_file.close()
 
     def _save_tutorial_info_files(self, tutorial_data: LyndaTutorialData):
         content_data = tutorial_data.get_content(format=LyndaTextFormat.MARKDOWN)
@@ -205,7 +279,6 @@ class Lynda:
         overview = open("OVERVIEW.md", "w")
         overview.write(overview_data)
         overview.close()
-
 
     def _download_chapters(self, tutorial_data: LyndaTutorialData):
         for chapter in tutorial_data.chapters:
@@ -231,7 +304,8 @@ class Lynda:
         os.chdir(folder)
 
         self._save_tutorial_info_files(tutorial_data)
-        #self._download_chapters(tutorial_data)
+        self._download_chapters(tutorial_data)
+        os.chdir("..")
 
         # print("DOWNLOADING IS DONE")
         # if settings.silent is None:
